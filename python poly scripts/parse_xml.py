bea5c04f10e3e80de7280ccb782e57275243a6fa
Nourhan Shafik <73674531+nshafik@users.noreply.github.com>
2022-12-13 13:35:16 +0200

    parse buildings from xml file

---
+++ python poly scripts/parse_xml.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import xml.etree.cElementTree as et
+
+tree=et.parse('osm.poly.xml')
+root=tree.getroot()
+print(root[2])
+
+root=tree.getroot()
+print(root.attrib)
+
+root=tree.getroot()
+
+polyDetails = []
+sub = 'amenity.school'
+
+print("--------------------------------")
+i = 0
+for poly in root:
+	try: 
+		if sub in (poly.attrib.get('type')):
+			# print(poly.attrib)
+			polyDetails.append(poly.attrib)
+	except: 
+		continue 
+
+df = pd.DataFrame.from_dict(polyDetails) 
+df.to_csv (r'gen.csv', index = False, header=True)
+
+
