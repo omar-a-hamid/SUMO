4fdfd60f22972f4f45e431fddc1c69612cff905e
nourhan <nourhannshafik@gmail.com>
2022-12-20 21:53:25 +0200

    parse edges and bus stops from xml files

---
+++ python poly scripts/parsedge.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import xml.etree.cElementTree as et
+
+tree=et.parse('nasrcity_osm.net.xml')
+root=tree.getroot()
+
+root=tree.getroot()
+print(root.iter('edge'))
+
+
+polyDetails = []
+
+print("--------------------------------")
+i = 0
+for poly in root.iter('edge'):
+	# i+=1
+	# print(i)
+# 	# print(poly.attrib.get('type') == sub)
+	# try: 
+	if (poly.attrib.get('shape')):
+		print(poly.attrib)
+		polyDetails.append(poly.attrib)
+# 			# np.average()
+# 			# print(average(poly.attrib.get('shape'), axis=0))
+
+# 	except: 
+# 		continue 
+
+df = pd.DataFrame.from_dict(polyDetails) 
+df.to_csv (r'edges.csv', index = False, header=True)
+
+
