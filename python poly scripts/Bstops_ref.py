a85d2742e674800430f218dc5c77d08705e26234
Omar Ahmed <omarahmedabdelhamid@outlook.com>
2023-01-25 14:43:47 +0200

    ref id dictionary

---
+++ python poly scripts/Bstops_ref.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("Bstop.csv")
+new_ref_id = []
+counter = 1
+for ref_id in range(len(df)):
+    new_ref_id.append(counter)
+    counter+=1
+# for row in df:
+    # print(row)
+df['ref_id'] = range(1, len(df) + 1)
+
+  
+
+df.to_csv("Bstop_ref_id.csv", index=False)
+  
+print(df)
